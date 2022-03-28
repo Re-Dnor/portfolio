64e09eab97a17eefae535fbddaadeb34520330d4
Re-Dnor <MokshinRE@yandex.ru>
2022-03-28 18:33:37 +0500

    Добавил анимацию

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const road = document.getElementById('road');
 const title = document.getElementById('title');
 
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     let value = window.scrollY;
 
     bg.style.top = value * 0.5 + 'px';
@@ -26,7 +26,7 @@ AOS.init({
     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 200, // offset (in px) from the original trigger point
+    offset: 150, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 1000, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
